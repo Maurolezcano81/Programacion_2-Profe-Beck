29c32d3b7a7faf2db2cc8ae2dec6a155268acca5
Maurolezcano81 <maurolezcano111@gmail.com>
2023-09-18 04:03:28 -0300

    Iniciando el MVC y la configuracion de la BD

---
+++ Tarea 3/config/conexion.php
@@ -0,0 +1,44 @@
+<?php
+
+class conexion{
+    private $_con;
+    private $username;
+    private $host;
+    private $password;
+    private $dbname;
+    private $port;
+
+    /* constructor de la base de datos*/ 
+    public function __construc($query)
+    {
+        $this->username = 'root';
+        $this->host = 'localhost';
+        $this->password = '';
+        $this->dbname = 'usuarios';
+        $this->port = 3306;
+    }
+
+
+
+    /* funcion para conectarse a la base de datos*/
+    public function conectar(){
+        $this->_con = new mysqli($this->host, $this->username, $this->password, $this->dbname, $this->port);
+    }
+
+    /* funcion para desconectarse de la base de datos*/
+    public function desconectar(){
+        $this->_con->close();
+    }
+
+    /* funcion para consultar a la base de datos*/
+    public function consultar($query){
+        $this->conectar();
+        $resultado = $this->_con->query($query);
+        $this->desconectar();
+        return $resultado;
+    }
+}
+
+
+
+?>
